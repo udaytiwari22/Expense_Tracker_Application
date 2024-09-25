97223f15520a54d6a6dfa9b7f4dfbf1c7ec3cbff
Uday Tiwari <udaytiwaricyef2001@gmail.com>
2024-09-25 18:20:01 +0530

    user_input

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:expense_tracker_app/models/expense.dart';
 
 class NewExpense extends StatefulWidget {
-  const NewExpense({super.key});
+  const NewExpense({super.key, required this.onAddExpense});
+
+  final void Function(Expense expense) onAddExpense;
 
   @override
   State<NewExpense> createState() {
@@ -54,9 +56,13 @@ class _NewExpenseState extends State<NewExpense> {
           ],
         ),
       );
-      return ;
+      return;
     }
-
+    widget.onAddExpense(Expense(
+        title: _titleController.text,
+        amount: enteredAmount,
+        date: _selectedDate!,
+        category: _selectedCategory));
   }
 
   @override
