404e4c38c3560e2b1f5936c3d9e67294e34b819c
Uday Tiwari <udaytiwaricyef2001@gmail.com>
2024-12-20 13:01:38 +0530

    updated

--- lib/widgets/chart/chart.dart
+++ lib/widgets/chart/chart.dart
@@ -34,22 +34,20 @@ class Chart extends StatelessWidget {
         MediaQuery.of(context).platformBrightness == Brightness.dark;
     return Container(
       margin: const EdgeInsets.all(16),
-      padding: const EdgeInsets.symmetric(
-        vertical: 16,
-        horizontal: 8,
-      ),
+      padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 8),
       width: double.infinity,
       height: 180,
       decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(8),
-          gradient: LinearGradient(
-            colors: [
-              Theme.of(context).colorScheme.onPrimaryFixed.withOpacity(0.3),
-              Theme.of(context).colorScheme.primary.withOpacity(0.0)
-            ],
-            begin: Alignment.bottomCenter,
-            end: Alignment.topCenter,
-          )),
+        borderRadius: BorderRadius.circular(8),
+        gradient: LinearGradient(
+          colors: [
+            Theme.of(context).colorScheme.onPrimaryFixed.withOpacity(0.3),
+            Theme.of(context).colorScheme.primary.withOpacity(0.0)
+          ],
+          begin: Alignment.bottomCenter,
+          end: Alignment.topCenter,
+        ),
+      ),
       child: Column(
         children: [
           Expanded(
